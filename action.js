4f3798eb48d7936d50f7419ec8728ccb409c2cf4
Chrispy <42500247+Camberden@users.noreply.github.com>
2020-07-21 20:26:19 -0400

    Update action.js
    Red coloration using the onkeyup event working.

--- action.js
+++ action.js
@@ -1,13 +1,32 @@
-var q = 0;
+let q = 0;
+
+window.onload = function() {
+    console.log("Running!");
+}
+
 
 function clickmeboy() {
     q = q + 1;
     document.getElementById("pedestal").innerHTML = q;
 }
 
-var si = setInterval(myTimer, 1000);
+const si = setInterval(printTimer, 1000);
 
-function myTimer() {
-  var d = new Date();
+function printTimer() {
+  let d = new Date();
   document.getElementById("miniclock").innerHTML = d.toLocaleTimeString();
 } 
+
+function sampleTypingTest() {
+    let para = "The quick brown fox jumps over the lazy dog.";
+    const typed = document.getElementById("sampleTypingField").value;
+    const marker = document.getElementById("pangram");
+
+    if (typed.substring(0, typed.length) == para.substring(0, typed.length)) {
+        marker.innerHTML = para.replace(typed, '<span style="color:red">' + typed + '</span>');
+    }
+    
+    if (typed == para) {
+        alert("Nice!");
+    }
+}
