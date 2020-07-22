c4103205435e1f7ea64eeffa9e8fbf8014e6e976
Camberden <bernsteincuevas@outlook.com>
2020-07-21 22:38:01 -0400

    Integrating Mocha's linter; conforming action.js to JS standards.

--- action.js
+++ action.js
@@ -1,33 +1,36 @@
+'use strict';
+/* exported clickmeboy sampleTypingTest */
+
 let q = 0;
 
-window.onload = function() {
-    console.log("Running!");
+window.onload = function(){
+	console.log('Running!');
+};
+
+function clickmeboy(){
+	q = q + 1;
+	document.getElementById('pedestal').innerHTML = q;
 }
 
-function clickmeboy() {
-    q = q + 1;
-    document.getElementById("pedestal").innerHTML = q;
+setInterval(printTimer, 1000);
+
+function printTimer(){
+	const d = new Date();
+	document.getElementById('miniclock').innerHTML = d.toLocaleTimeString();
 }
 
-const si = setInterval(printTimer, 1000);
+function sampleTypingTest(){
+	const para = 'The quick brown fox jumps over the lazy dog.';
+	const typed = document.getElementById('sampleTypingField').value;
+	const marker = document.getElementById('pangram');
 
-function printTimer() {
-  let d = new Date();
-  document.getElementById("miniclock").innerHTML = d.toLocaleTimeString();
-} 
+	if (typed.substring(0, typed.length) === para.substring(0, typed.length)){
+		marker.innerHTML = para.replace(typed, '<span style="color:red">' + typed + '</span>');
+	}
 
-function sampleTypingTest() {
-    let para = "The quick brown fox jumps over the lazy dog.";
-    const typed = document.getElementById("sampleTypingField").value;
-    const marker = document.getElementById("pangram");
-
-    if (typed.substring(0, typed.length) == para.substring(0, typed.length)) {
-        marker.innerHTML = para.replace(typed, '<span style="color:red">' + typed + '</span>');
-    }
-    
-    if (typed == para) {
-        document.getElementById("sampleTypingField").value = "";
-        marker.innerHTML = para;
-        alert("Nice!");
-    }
+	if (typed === para){
+		document.getElementById('sampleTypingField').value = '';
+		marker.innerHTML = para;
+		alert('Nice!');
+	}
 }
