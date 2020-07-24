a0be18781b5cccfdee3dbd703d5f73b8f797a450
Camberden <bernsteincuevas@outlook.com>
2020-07-24 03:21:24 -0400

    JS Class dabbling.

--- demogame/demogame.js
+++ demogame/demogame.js
@@ -7,3 +7,27 @@ function printTimer(){
 	const d = new Date();
 	document.getElementById('miniclock').innerHTML = d.toLocaleTimeString();
 }
+
+
+class Player {
+	constructor(gamename, health, attack, defense){
+		this.playerName = gamename;
+		this.playerHealth = health;
+		this.playerAttack = attack;
+		this.playerDefense = defense;
+	}
+
+	static damage(player){
+		player.health--;
+	}
+	static perish(player){
+		alert('Oh my, you died.');
+		player.innerHTML = '';
+	}
+}
+
+let none = new Player ('', 0, 0, 0);
+none.innerHTML = '';
+// let prot = new Player('Protagonist', 10, 2, 0);
+// let scrib = new Player('Scrib', 5, 1, 0);
+
