0f5e2686c8869819e2b1a39dca22b17ff9b9037c
Camberden <bernsteincuevas@outlook.com>
2021-08-11 04:38:04 -0400

    Completed functionality for the Life in Weeks event listener for individual moments. Integrated iframes.

--- lifeinweeks/lifeinweeks.js
+++ lifeinweeks/lifeinweeks.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-
 function populateBoxes(){
 	console.log('Running lifeinweeks.js!');
 	const inception = 753300000000;
@@ -16,24 +15,49 @@ function populateBoxes(){
 		const span = document.createElement('span');
 		span.classList.add('momentbox');
 		span.id = `box${i}`;
-		// debugger;
 		return span;
 	});
 
-	//SEGMENTS
+	// --- BEGIN MOMENTS SECTION ---
 
 	//Filled
 	spans.forEach((span, i) => {
 		if (i < weekslived)
 			span.classList.add('momentboxfilled');
 	});
+	//First Year
 	spans.forEach((span, i) => {
-		if (i < weeks)
+		if (i < weeks){
 			span.classList.add('firstyearmoment');
+			span.addEventListener('click', function(){
+				displayMomentInfo('firstyearmoment');
+			}, false);
+		}
 	});
+	//NC Relocation
+	spans.forEach((span, i) => {
+		if (i === 1395){
+			span.classList.add('ncrelocationmoment');
+			span.addEventListener('click', function(){
+				displayMomentInfo('ncrelocationmoment');
+			}, false);
+		}
+	});
+
+	// --- END MOMENTS SECTION ---
 	const boxinput = document.getElementById('boxinput');
 	spans.forEach(s => boxinput.appendChild(s));
-	//debugger;
 }
 
+function displayMomentInfo(moment){
+	const iframetext = '<iframe frameborder="0" style="height: 185px; overflow:scroll; width: 100%' +
+		'marginheight="1" marginwidth="1" seamless="seamless" scrolling="no" frameborder="0"' +
+		'allowtransparency="true" src=../lifeinweeks/moments/';
+	if (moment === 'firstyearmoment'){
+		document.getElementById('moment-info').innerHTML = iframetext + 'firstyear.html></iframe';
+	}
+	if (moment === 'ncrelocationmoment'){
+		document.getElementById('moment-info').innerHTML = iframetext + 'ncrelocation.html></iframe>';
+	}
+}
 populateBoxes();
