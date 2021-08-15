2f7b5542a41e372ceace4f1dff6b407df49dd112
Camberden <bernsteincuevas@outlook.com>
2021-08-15 11:01:09 -0400

    Further progress on Personal Record. Included cost per credit display and links

--- personalrecord/personalrecord.js
+++ personalrecord/personalrecord.js
@@ -4,21 +4,26 @@
 window.onload = () => console.log('Running!');
 
 const cpccourses = document.querySelectorAll('.cpcreq');
-console.log(cpccourses);
 const gcpcourses = document.querySelectorAll('.cpcreq, .gcpreq');
-console.log(gcpcourses);
 const mcscourses = document.querySelectorAll('.cpcreq, .gcpreq, .mcsreq');
-console.log(mcscourses);
 
+const aas = document.getElementById('aas');
 const cpc = document.getElementById('cpc');
 const gcp = document.getElementById('gcp');
 const mcs = document.getElementById('mcs');
+
+console.log(mcs);
+
+aas.onmouseenter = function(){
+	displayCredits(aas);
+};
 
 cpc.onmouseenter = function(){
 	for (let i = 0; i < cpccourses.length; ++i){
 		const course = cpccourses[i];
 		course.classList.add('cpcreqhighlight');
 	}
+	displayCredits(cpc);
 };
 cpc.onmouseleave = function(){
 	for (let i = 0; i < cpccourses.length; ++i){
@@ -31,6 +36,7 @@ gcp.onmouseenter = function(){
 	for (let i = 0; i < gcpcourses.length; ++i){
 		const course = gcpcourses[i];
 		course.classList.add('gcpreqhighlight');
+		displayCredits(gcp);
 	}
 };
 gcp.onmouseleave = function(){
@@ -44,6 +50,7 @@ mcs.onmouseenter = function(){
 	for (let i = 0; i < mcscourses.length; ++i){
 		const course = mcscourses[i];
 		course.classList.add('mcsreqhighlight');
+		displayCredits(mcs);
 	}
 };
 mcs.onmouseleave = function(){
@@ -52,3 +59,31 @@ mcs.onmouseleave = function(){
 		course.classList.remove('mcsreqhighlight');
 	}
 };
+
+function displayCredits(curriculum){
+	switch (curriculum){
+		case aas:
+			document.getElementById('credit-info').innerHTML =
+				'<p>CREDIT HOUR w/o FEES: $76<br>' +
+				'PER 3 CREDIT COURSE: $357.00<br>';
+			break;
+		case cpc:
+			document.getElementById('credit-info').innerHTML =
+				'<p>CREDIT HOUR w/o FEES: $272.29<br>' +
+				'PER 3 CREDIT COURSE: $1066.89<br>' +
+				'FOR ALL 22 CREDITS: $7740.52';
+			break;
+		case gcp:
+			document.getElementById('credit-info').innerHTML =
+				'<p>CREDIT HOUR w/o FEES: $505.28<br>' +
+				'PER 3 CREDIT COURSE: $2,782.53<br>' +
+				'FOR ALL 12 CREDITS: $11130.12';
+			break;
+		case mcs:
+			document.getElementById('credit-info').innerHTML =
+				'<p>CREDIT HOUR w/o FEES: $505.28<br>' +
+				'PER 3 CREDIT COURSE: $2,782.53<br>' +
+				'FOR ALL 19 CREDITS $19982,99';
+			break;
+	}
+}
