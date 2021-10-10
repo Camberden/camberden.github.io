3fca33e597f75ab0db2105040de20dc2f4d6b0d4
Camberden <bernsteincuevas@outlook.com>
2021-10-10 05:42:00 -0400

    Added hover animation for music - the same as in the index. Added a second tune to select from, and staged the project in preparation to make the functions tune-agnostic, being able to play the right tune without running a lengthy if statement.

--- music/music.js
+++ music/music.js
@@ -4,6 +4,7 @@
 window.onload = () => console.log('Running!');
 
 const meaning = new Audio('../assets/Meaning.mp3');
+const woai = new Audio('../assets/WarOfArtInstrumental.mp3');
 const playtext = document.getElementById('meaningtune');
 
 function playTune(){
@@ -25,16 +26,22 @@ function stopPlay(){
 	setTimeout(() => document.getElementById('meaningtune').style = 'color: inherit;', 4000);
 }
 
+
+// THIS FUNCTION NEEDS TO BECOME TUNE AGNOSTIC.
+// MAKE IT SUCH THAT SELECTING A TUNE DOES NOT REQUIRE AN EXTENDING IF STATEMENT.
+// PERHAPS QUERY SELECT THE TEXT IN THE BOX AND RETRIEVE THE MP3 FILE THROUGH IT.
 playtext.addEventListener('click', function(){
 	++playcounter;
 	console.log(playcounter);
 	if (playcounter % 2){
 		meaning.play();
 		document.getElementById('meaningtune').classList.add('currentlyplaying');
+		document.getElementById('track1').classList.add('currentlyplayingbox');
 	}
 	else {
 		meaning.pause();
 		meaning.currentTime = 0;
-		document.getElementById('meaningtune').classList.remove('currentlyplaying');
+		document.getElementById('warofartinstrumentaltune').classList.remove('currentlyplaying');
+		document.getElementById('track1').classList.remove('currentlyplayingbox');
 	}
 });
