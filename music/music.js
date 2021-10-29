464a4014aa6665af71cb856806b94072fc82e3a8
Camberden <bernsteincuevas@outlook.com>
2021-10-29 06:39:35 -0400

    Added lyrics bar for music during playtime, showing the current lines being sung.

--- music/music.js
+++ music/music.js
@@ -6,25 +6,12 @@ window.onload = () => console.log('Running!');
 const meaning = new Audio('../assets/Meaning.mp3');
 const woai = new Audio('../assets/WarOfArtInstrumental.mp3');
 const playtext = document.getElementById('meaningtune');
+const credit = document.getElementById('credit');
+// ASSIGN THE VARIABLE TO THE LYRICS FUNCTION BASED ON THE TUNE CLICKED UPON
+const currentlyrics = document.getElementById('meaning-lyrics');
 
-function playTune(){
-	document.getElementById('demotxt').style = 'color:red;';
-	meaning.play();
-	console.log('Playing!');
-	// stopPlay();
-}
-
-// meaning.addEventListener('ended', function(){
-// 	meaning.currentTime = 0;
-// 	console.log('meaning ended');
-// 	document.getElementById('demotxt').style = 'font-size:inherit; color:inherit;';
-// });
 
 let playcounter = 0;
-
-function stopPlay(){
-	setTimeout(() => document.getElementById('meaningtune').style = 'color: inherit;', 4000);
-}
 
 
 // THIS FUNCTION NEEDS TO BECOME TUNE AGNOSTIC.
@@ -37,11 +24,37 @@ playtext.addEventListener('click', function(){
 		meaning.play();
 		document.getElementById('meaningtune').classList.add('currentlyplaying');
 		document.getElementById('track1').classList.add('currentlyplayingbox');
+		setInterval(function(){
+			meaningLyrics();
+		}, 500);
 	}
 	else {
 		meaning.pause();
 		meaning.currentTime = 0;
 		document.getElementById('warofartinstrumentaltune').classList.remove('currentlyplaying');
 		document.getElementById('track1').classList.remove('currentlyplayingbox');
+		currentlyrics.innerHTML = '';
+		credit.innerHTML = '';
 	}
 });
+
+function meaningLyrics(){
+	const ts = meaning.currentTime;
+	if (ts === 0){
+		currentlyrics.innerHTML = '';
+	}
+	if (ts < 22 && ts > 0){
+		currentlyrics.innerHTML = 'huff...';
+		credit.innerHTML = '<br><br>Chrispy 2021';
+	}
+	if (ts > 22 && ts < 24){
+		currentlyrics.innerHTML = 'Another day...';
+	}
+	if (ts > 24 && ts < 26){
+		currentlyrics.innerHTML = ' I\'m all alone';
+	}
+	if (ts > 26 && ts < 30){
+		currentlyrics.innerHTML = ' I wonder what to do again...';
+	}
+}
+
