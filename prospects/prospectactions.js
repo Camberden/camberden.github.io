f22a5cb030ea0093b8235bb7b4dc014e03f1b12a
Christian N. Bernstein-Cuevas <chrispy@Christians-MBP.lan>
2022-05-08 22:40:21 -0400

    More overall site refactoring; added links; deleted old prospect page data.

--- prospects/prospectactions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-printTimer();
-setInterval(printTimer, 1000);
-
-function printTimer(){
-	const d = new Date();
-	document.getElementById('miniclock').innerHTML = d.toLocaleTimeString();
-}
