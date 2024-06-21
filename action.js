c370e7fde4f205db6c1eb65400ae03402fbea163
Camberden <42500247+Camberden@users.noreply.github.com>
2024-06-21 00:38:50 -0400

    Added acc positions to interesting jobs section

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 window.onload = () => console.log("Running!");
-const lastestUpdatePushed = "Sunday June 16 2024";
+const lastestUpdatePushed = "Friday June 21 2024";
 
 // --- INFO AND ENTRYWAYS --- //
 
