82fb082a9408ecbc0ca8fd1ee5450589b0c27412
Camberden <42500247+Camberden@users.noreply.github.com>
2024-08-12 00:31:43 -0400

    Blogging is more navigable

--- musings/musings.js
+++ musings/musings.js
@@ -452,45 +452,63 @@ const boddhisattvaPrecepts = [
 ];
 
 // ----- BLOG POSTS
-// console.log(blogData[0]);
 let activeBlogPost = document.getElementById("active-blog-post");
 let activeBlogPostNumber = blogData.length - 1;
 let blogPostList = document.getElementById("blog-post-list");
 
-function initBlogData(){
-	for (i = 0; i < blogData.length; i++){
-		blogData[i] = `<span id=entry-${i}>` + blogData[i] + `</span>`;
+function displayActiveBlogPostNumber(){
+	document.getElementById("displayed-post").innerHTML = activeBlogPostNumber + 1;
+}
+
+function extractHeaderData(increment){
+	const instance = blogData[increment];
+	let splitInstance = instance.split("|");
+	let instanceDate = splitInstance[1].trim();
+	document.getElementById("blog-post-date").innerHTML = instanceDate;
+	let instanceLocation = splitInstance[2].trim();
+	document.getElementById("blog-post-location").innerHTML = instanceLocation;
+	let instanceTime = splitInstance[3].trim();
+	document.getElementById("blog-post-time").innerHTML = instanceTime;
+	let instanceBlogPost = splitInstance[4].trim();
+	displayActiveBlogPostNumber();
+
+return instanceBlogPost;
+}
+
+function initBlogData(dataLength){
+	for (i = 0; i <= dataLength - 1; i++){
 		let listedBlogPost = document.createElement("li");
 		blogPostList.appendChild(listedBlogPost);
 		listedBlogPost.append(`Post ${i + 1}`);
+
+		// Latest Entry
+		if (i === dataLength - 1) {
+			activeBlogPost.innerHTML = `<span id=entry-${activeBlogPostNumber}>` + extractHeaderData(activeBlogPostNumber) + `</span>`;
+			displayActiveBlogPostNumber();
+		}
 	}
-	activeBlogPost.innerHTML = blogData[activeBlogPostNumber];
 }
-initBlogData();
+initBlogData(blogData.length);
 
-function setActiveBlogPost(choice){
-	activeBlogPost.innerHTML = blogData[0];
+function chooseActiveBlogPost(choice){
+	if (choice <= blogData.length && choice >= 0) {
+		activeBlogPostNumber = choice;
+		activeBlogPost.innerHTML = `<span id=entry-${activeBlogPostNumber}>` + extractHeaderData(activeBlogPostNumber) + `</span>`;
+	}
 }
-
-console.log(`Index Number: ` + (blogData.length - 1));
-console.log(`Active Number:` + activeBlogPostNumber);
-console.log(activeBlogPostNumber == (blogData.length - 1) ? true : false);
 
 function previousBlogPost(){
 	if (activeBlogPostNumber > 0) {
 		activeBlogPostNumber--;
-		console.log(`Active Number:` + activeBlogPostNumber);
-		console.log(`Index Number: ` + (blogData.length - 1));
-		activeBlogPost.innerHTML = blogData[activeBlogPostNumber];
+		activeBlogPost.innerHTML = `<span id=entry-${activeBlogPostNumber}>` + extractHeaderData(activeBlogPostNumber) + `</span>`;
 	}
 }
 
 function nextBlogPost(){
 	if (activeBlogPostNumber < blogData.length - 1) {
 		activeBlogPostNumber++;
-		console.log(`Active Number:` + activeBlogPostNumber);
-		console.log(`Index Number: ` + (blogData.length - 1));
-		activeBlogPost.innerHTML = blogData[activeBlogPostNumber];
+		activeBlogPost.innerHTML = `<span id=entry-${activeBlogPostNumber}>` + extractHeaderData(activeBlogPostNumber) + `</span>`;
 	}
 }
+
 
