fd6bc397a46598d90cc6049d161679363cf1c46f
Camberden <42500247+Camberden@users.noreply.github.com>
2024-08-24 01:20:44 -0400

    Populated quick links at dashboard

--- action.js
+++ action.js
@@ -1,23 +1,11 @@
 window.onload = () => console.log("Running!");
-const lastestUpdatePushed = "Friday August 23rd, 2024";
+const lastestUpdatePushed = "Saturday August 24th, 2024";
 
 // --- INFO AND ENTRYWAYS --- //
-
-const musiclink = `<a href="music/music.html">`;
-const musingslink = `<a href="musings/musings.html">`;
-const personalrecordlink = `<a href="personalrecord/personalrecord.html">`;
-const lifeinweekslink = `<a href="lifeinweeks/lifeinweeks.html">`;
-const dashboardlink = `<a href="dashboard/dashboard.html">`;
-const linkending =`Enter</a>`;
-
-const music = document.getElementById("music");
-const musings = document.getElementById("musings");
-const dashboard = document.getElementById("dashboard");
 
 const latestUpdate = document.getElementById("latest-update");
 latestUpdate.innerHTML = lastestUpdatePushed;
 
-const menuItems = document.querySelectorAll(".section-title");
 const iconPlay = document.querySelector("#camberden");
 
 iconPlay.onclick = function(){
