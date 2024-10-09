5fcc704122e2bb59eb353a76a9f68647e216187f
Camberden <42500247+Camberden@users.noreply.github.com>
2024-10-09 01:07:26 -0400

    Updated action.js in index to defer; bad loading from .io

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 window.onload = () => console.log("Running!");
-const lastestUpdatePushed = "Saturday October 5th, 2024";
+const lastestUpdatePushed = "Wednesday October 9th, 2024";
 
 // --- INFO AND ENTRYWAYS --- //
 
