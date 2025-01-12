da2cd54c6245a9076d3ab7e35d9f33711160a289
Camberden <42500247+Camberden@users.noreply.github.com>
2025-01-11 21:32:51 -0500

    refined acc quiz module w/ acronyms quiz

--- accounting/accounting.js
+++ accounting/accounting.js
@@ -109,9 +109,6 @@ function initAccountingNotes(tag, attributes) {
 }
 initAccountingNotes("mark", `class="formula"`);
 
-console.log(accountingChapters[4].formulas[0]);
-
-
 // parseAccountingFormulas("mark", `class="formula"`);
 
 
@@ -123,7 +120,12 @@ function promptAccountingFormula() {
 }
 promptAccountingFormula();
 
-function loadQuiz() {
+
+
+
+// ----- QUIZ MODULES ----- //
+
+function formulaQuiz() {
 	const textArea = document.createElement("textarea");
 	textArea.setAttribute("id", "quiz-field");
 	const accountingQuizText = document.getElementById("accounting-quiz-text");
@@ -148,3 +150,115 @@ function loadQuiz() {
 	showAnswerButton.appendChild(text);
 	showAnswer.appendChild(showAnswerButton);
 }
+
+const acronyms = [
+	"GAAP", "SEC", "FASB", "SFAC", "SFAS", "SFAS interpolations", "SFAS Staff Positions",
+	"EITF", "FAF", "FASAC", "ASU", "ASC", "PCC", "GASB", "FAF", "GASAC", "AICPA", "CAP", "APB", "AcSEC",
+	"FinREC", "ASB", "ASB SAS", "PCAOB", "IASB", "AAA",
+];
+const acronymMeanings = ["Generally Accepted Accounting Principles", "Securities & Exchange Commission",
+	"Financial Accounting Standards Board", "Statements of Financial Accounting Concepts", 
+	"Statements of Financial Accounting Standards", "Pre-codification clarifications", "Pre-codification clarifications",
+	"Emerging Issues Task Force", "Financial Accounting Foundation", "Financial Accounting Standards Advisory Council",
+	"Accounting Standards Update", "Accounting Standards Codification", "Private Company Council", 
+	"Governmental Accounting Standards Board", "Financial Accounting Foundation", "Governmental Accounting Standards Advisory Council",
+	"American Institute of Certified Public Accountants", "Committee on Accounting Procedure", "Accounting Principles Board",
+	"Accounting Standards Executive Committee", "Financial Reporting Executive Committee", "Auditing Standards Board",
+	"Statements on Auditing Standards (issued by ASB)", "Public Company Accounting Oversight Board",
+	"International Accounting Standards Board", "American Accounting Association",
+];
+const acronymSignificance = [
+	"The general term for rules to follow in accounting practice",
+	"Federal government agency with full authority to set standards: "  + 
+	"Issues FRR's (Financial Reporting Releases) [Positions on Issues] " +
+	"Issues SAB's (Staff Accounting Bulletins) [Interpretations]",
+	"Primary Private Sector accounting standard-setting body",
+	"Forms the Conceptual Framework for accounting",
+	"Formerly the primary output of FASB (pre-codification)",
+	"(modify / extend existing standards",
+	"(interpretive guidance / minor amendments)",
+	"Issues Consensus Opinions on fast-changing issues",
+	"Appointed the FASB",
+	"Advises the FASB",
+	"Primary output of FASB due process (new rules issued)",
+	"The centralized searchable database of GAAP",
+	"Advisory body for FASB on private company matters",
+	"State & Local Government accounting standards",
+	"Appointed the GASB", 
+	"Advises the GASB",
+	"Private Sector advocate for accounting profession",
+	"Issued ARB's (Accounting Research Bulletins",
+	"Issued APBO's (Accounting Principles Board Opinions",
+	"Speaks for the AICPA and has issued: " +
+	"Audit & Accounting Guides (specialized guidance) " +
+	"SOP (Statement of Position) & Practice Bulletins",
+	"Replaced the AcSEC",
+	"Private company audit standards",
+	"(still in force & growing)",
+	"Oversees and creates auditing standards for public companies", 
+	"Issues IFRS (International Financial Reporting Standards)",
+	"Accounting Educator Group (input for standard creation)",
+];
+
+function acronymQuiz() {
+	const textArea = document.createElement("textarea");
+	textArea.setAttribute("id", "quiz-field");
+	const accountingQuizText = document.getElementById("accounting-quiz-text");
+	const quizInput = document.getElementById("quiz-input");
+	const showAnswer = document.getElementById("show-answer");
+	quizInput.innerHTML = "";
+	quizInput.appendChild(textArea);
+	showAnswer.innerHTML = "";
+	let r = Math.random();
+	let acronymRandom = Math.round(acronyms.length * r);
+	let problem = acronyms[acronymRandom];
+	let answer = acronymMeanings[acronymRandom];
+	let significance = acronymSignificance[acronymRandom];
+
+	accountingQuizText.innerHTML = problem;
+	const showAnswerButton = document.createElement("button");
+	showAnswerButton.onclick = function () {
+		accountingQuizText.innerHTML = problem + "&nbsp" + answer + "&nbsp" + significance;
+		quizInput.innerHTML = document.getElementById("quiz-field").value;
+		showAnswer.innerHTML = "";
+	}
+	const text = document.createTextNode("Show Answer");
+	showAnswerButton.appendChild(text);
+	showAnswer.appendChild(showAnswerButton);
+}
+
+function exitQuiz() {
+	document.getElementById("accounting-quiz-text").innerHTML = "Accounting Quizes";
+	document.getElementById("quiz-input").innerHTML = "";
+	document.getElementById("show-answer").innerHTML = "";
+}
+
+function initQuizButtons() {
+	document.querySelectorAll(".accounting-quiz-button").forEach(button => {
+	button.onclick = function() {
+		ButtonInterface.buttonOnClick(button);
+		switch(button.value) {
+			case "formula-quiz":
+				formulaQuiz();
+			break;
+			case "acronym-quiz":
+				acronymQuiz();
+			break;
+			case "exit-quiz":
+				exitQuiz();
+			break;
+			default:
+				exitQuiz();
+		}
+	}
+
+	button.onmouseenter = function() {
+		ButtonInterface.buttonOnMouseEnter(button);
+	}
+	button.onmouseleave = function() {
+		ButtonInterface.buttonOnMouseLeave(button);
+	}
+
+	});
+}
+initQuizButtons();
