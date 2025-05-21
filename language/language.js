bd885d5025db504f66de9ddb2b4a4eb9fb864a81
Camberden <42500247+Camberden@users.noreply.github.com>
2025-05-21 03:03:45 -0400

    redoing language page; tables in progress

--- language/language.js
+++ language/language.js
@@ -2,18 +2,18 @@ window.onload = () => console.log("Running!");
 
 /**
  * @param {string[]} vocabulary - contains 10 elements
- * @param {string[]} english - contains 10 elements
  * @param {string} grouping - topic
  * @param {string} language - language selected
  */
-class StudyModule {
-	constructor(vocabulary, english, grouping, language) {
+class VocabularyModule {
+	constructor(vocabulary, grouping, language) {
 		this.vocabulary = vocabulary;
-		this.english = english;
 		this.grouping = grouping;
 		this.language = language;
 	}
 }
+
+
 
 const groupingField = document.getElementById("grouping-field");
 const vocabField = document.getElementById("vocab-field");
@@ -21,221 +21,153 @@ const englishField = document.getElementById("english-field");
 const sentenceField = document.getElementById("sentence-field");
 const transliterationField = document.getElementById("transliteration-field");
 
-const currentGroupingItem = document.getElementById("current-grouping-item");
-const currentGroupingTotal = document.getElementById("current-grouping-total");
-
 const jishoField = document.getElementById("jisho-field");
 const jishoIFrame = document.getElementById("jisho-frame");
 
-let selectedItemNumber = 0;
-let activeModules = [];
-let selectedModule;
-
 const languageButtons = document.querySelectorAll(".language-button");
+
+
 
 // ----- JAPANESE ----- //
 
-const testModuleVocabulary = [
-	"記載「きさい」する",
-	"情報「じょうほう」",
-	"客室乗務員「きゃくしつじょうむいん」",
-	"知「し」らせる",
-	"示「しめ」し",
-	"筋「すじ」が通「とう」る",
-	"並「なら」べる",
-	"食卓「しょくたく」",
-	"姿勢「しせい」を正「ただす」す",
-	"夜間「やかん」",
-];
-
-const testModuleEnglish = [
-	"to mention (in a document); a record, an entry, a statement",
-	"information; news; report",
-	"flight attendant",
-	"to notify; to advise; to inform",
-	"a lesson; discipline; an (bad or good) example",
-	"to make sense; to be consistent; to be logical",
-	"to line up; to enumerate; to be equal to",
-	"dining table",
-	"to straighten oneself",
-	"nighttime",
-];
-
 const moduleJp1 = [
-	"最も",
-	"才能",
-	"すでに",
-	"かつて",
-	"準備",
-	"許す",
-	"表現",
-	"想像",
-	"現在",
-	"割って入る",
-	"乾かす",
-	"展望台",
-	"ぶつがる",
-	"居酒屋",
-	"足を組む",
-	"眺める",
-	"逃げる",
-	"商品",
-	"該当",
-	"冒頭",
-	"見本",
-];
-
-const moduleJpEn1 = [
-	"the most",
-	"talent",
-	"already",
-	"used to",
-	"preparation; readiness",
-	"to forgive; to allow",
-	"expression",
-	"imagination; supposition",
-	"at present",
-	"to interrupt",
-	"to dry",
-	"observation deck",
-	"to bump into",
-	"tavern",
-	"to cross legs",
-	"to gaze",
-	"to escape",
-	"merchandise",
-	"corresponding to; queried for",
-	"outset",
-	"sample",
-]
-
-const moduleJpEn2 = [
-	"to be late",
-	"slow",
-	"to be afraid of (畏)",
-	"to occur/happen",
-	"to get angry",
-	"to wake up",
-	"to be afraid of",
-	"to send",
-	"to place",
-	"late",
-]
-
-const moduleJp2 = [
-	"遅れる okureru",
-	"遅い osoi",
-	"恐れる osoreru",
-	"起こる okoru",
-	"怒る osoru",
-	"起きる ikiru",
-	"怖がる kowagaru",
-	"送る okiru",
-	"置く oku",
-	"遅く osoku (adv)",
+	"遅れる|to be late",
+	"遅い|slow",
+	"恐れる|to be afraid of(畏)",
+	"起こる|to occur/happen",
+	"怒る|to get angry",
+	"起きる|to wake up",
+	"怖がる|to be afraid of",
+	"送る|to send",
+	"置く|to place",
+	"遅く|late (adv)",
 
 ];
+// ----- EREMORAN ----- //
+// ----- SPANISH ----- //
+// ----- POLISH ----- //
 
+const vocabularyModules = [
+	okoOsoConfusion = new VocabularyModule(moduleJp1, "Oko-Oso Confusion", "jp"),
+];
+
+// Create Map for removal quiz?
+
+
+const objectTemplate = ["language", "english"]
+
+
+/**
+ * @param {VocabularyModule} module
+ */
+function generateStudyTable(module) {
+	const span = document.createElement("span");
+	const text = document.createTextNode(module.grouping);
+	span.appendChild(text);
+	const studyTable = document.getElementById("study-tables");
+	const table = document.createElement("table");
+	table.appendChild(span);
+	const tr = document.createElement("tr");
+
+	for (let i = 0; i < objectTemplate.length; i++){
+		const th = document.createElement("th");
+		let language = "Language";
+		switch (module.language) {
+			case "jp":
+				language = "Japanese";
+			break;
+			case "er":
+				language = "Eremoran";
+			break;
+			case "es":
+				language = "Spanish";
+			break;
+			case "pl":
+				language = "Polish";
+			break;
+			default:
+				language = "Language";
+			break;
+		}
+		const text = document.createTextNode(language);
+		th.appendChild(text);
+		tr.appendChild(th);
+	}
+	table.appendChild(tr);
+
+	for (let i = 0; i < module.vocabulary.length; i++) {
+		const languageData = module.vocabulary[i].split("|");
+		const tr = document.createElement("tr");
+		const fl = document.createElement("td");
+		const flText = document.createTextNode(languageData[0]);
+		fl.appendChild(flText);
+		const en = document.createElement("td");
+		const enText = document.createTextNode(languageData[1]);
+		en.appendChild(enText);
+
+		tr.appendChild(fl);
+		tr.appendChild(en);
+		table.appendChild(tr);
+	}
+
+
+
+	studyTable.appendChild(table);
+
+}
+
+generateStudyTable(vocabularyModules[0]);
 
 
 // ----- EREMORAN ----- //
 
-const testModuleEremoranVocabulary = [
-	"numz",
-	"rurauk",
-	"uluk",
-	"ad",
-	"abat",
-	"balitz",
-	"eremor",
-	"ômorz",
-	"lusik",
-	"morôz",
-	"busk",
-];
+// ----- SPANISH ----- //
 
-const testModuleErEn1 = [
-	"to run",
-	"cat",
-	"black",
-	"on/at/onto",
-	"table",
-	"to break",
-	"Eremor (place)",
-	"to know",
-	"wolf",
-	"to say",
-	"dog",
-];
+// ----- POLISH ----- //
+
+
 
 
 // ----- LOGIC FUNCTIONS ----- //
 
-const studyModules = [
-	jp1 = new StudyModule(moduleJp1, moduleJpEn1, "rhodia book 1", "jp"),
-	jp2 = new StudyModule(moduleJp2, moduleJpEn2, "oso-oko confusion", "jp"),
-	er1 = new StudyModule(testModuleEremoranVocabulary, testModuleErEn1, "demo-er-grouping", "er"),
-];
 
-function loadModuleItem(module, itemNumber) {
-	if (module.language === "er"){
-		transliterationField.textContent = module.vocabulary[itemNumber];
-	} else {
-		transliterationField.textContent = "";
-	}
-	groupingField.textContent = module.grouping;
-	vocabField.textContent = module.vocabulary[itemNumber];
-	englishField.textContent = module.english[itemNumber];
-	currentGroupingItem.textContent = itemNumber + 1;
-	currentGroupingTotal.textContent = module.vocabulary.length;
-}
 
-function generateStudyModules(selectedLanguage) {
 
-	while (activeModules.length) {
-		activeModules.pop();
-		console.log("Popping array.");
-	}
+// function generateStudyModules(selectedLanguage) {
 
-	for (let studyModule of studyModules) {
-		if (studyModule.language === selectedLanguage) {
-			if (studyModule.language === "er") {
-				vocabField.classList.add("eremoran-kiptascript");
-			}
-			activeModules.push(studyModule);
-		}
-	}
-	selectedModule = activeModules[0];
-	loadModuleItem(selectedModule, selectedItemNumber);
-}
-generateStudyModules("jp");
+// 	while (activeModules.length) {
+// 		activeModules.pop();
+// 		console.log("Popping array.");
+// 	}
+
+// 	for (let studyModule of studyModules) {
+// 		if (studyModule.language === selectedLanguage) {
+// 			if (studyModule.language === "er") {
+// 				vocabField.classList.add("eremoran-kiptascript");
+// 			}
+// 			activeModules.push(studyModule);
+// 		}
+// 	}
+// 	selectedModule = activeModules[0];
+// 	loadModuleItem(selectedModule, selectedItemNumber);
+// }
+// generateStudyModules("jp");
 
 function enableLanguageButtons(){
 	languageButtons.forEach(button => {
 		button.onclick = function(){
 			ButtonInterface.buttonOnClick(button);
 			switch (button.value) {
-				case "japanese":
-					if (activeModules.language != "jp") {
-						generateStudyModules("jp");
-					}
-					break;
-				case "eremoran":
-					if (activeModules.language != "er") {
-						generateStudyModules("er");
-					}
-				case "next":
-					if (selectedItemNumber < selectedModule.vocabulary.length - 1) {
-						selectedItemNumber++;
-						loadModuleItem(selectedModule, selectedItemNumber);
-					}
-					break;
-				case "previous":
-					if (selectedItemNumber >= 1) {
-						selectedItemNumber--;
-						loadModuleItem(selectedModule, selectedItemNumber);
-					}
-					break;
+				// case "japanese":
+				// 	if (activeModules.language != "jp") {
+				// 		generateStudyModules("jp");
+				// 	}
+				// 	break;
+				// case "eremoran":
+				// 	if (activeModules.language != "er") {
+				// 		generateStudyModules("er");
+				// 	}
+				// 	break;
 				default:
 					console.log(activeModules.length);
 					break;
