c4428bbd8fc3f8a61cfd0f0305c990c911da4bd3
Camberden <42500247+Camberden@users.noreply.github.com>
2025-05-21 00:44:24 -0400

    Reworked date calculator at action

--- action.js
+++ action.js
@@ -2,17 +2,26 @@ window.onload = () => console.log("Running!");
 const latestUpdatePushed = "Wednesday May 21st, 2025";
 const splitDate = latestUpdatePushed.split(" ");
 
-function convertDate(m, d, y){
-	const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	const day = d.replace(/[^0-9]/g, ' ');
-	const year = parseInt(y);
-	const date = new Date(year, months.indexOf(m), parseInt(day));
+/**
+ * 
+ * @param {string} month
+ * @param {string} calendarDay
+ * @param {string} year 
+ * @returns date Object
+ * @description Converts my chosen date format for update display into a date Object
+ */
+function convertDate(month, calendarDay, year){
+	const convertedMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	const convertedCalendarDay = calendarDay.replace(/[^0-9]/g, ' ');
+	const convertedYear = parseInt(year);
+	const date = new Date(convertedYear, convertedMonth.indexOf(month), parseInt(convertedCalendarDay));
 	
 	return date;
 }
 const formattedDate = convertDate(splitDate[1], splitDate[2], splitDate[3]);
 /**
- * @param {Date} date 
+ * @param {Date} date
+ * @description Converts date object to Japanese date
  */
 function convertToJapaneseDate(date) {
 	let japaneseDate = "最近アップデート令和";
@@ -39,18 +48,23 @@ function convertToJapaneseDate(date) {
 	}
 
 	// ----- DAYS ----- //
+	console.log(date.getDate());
 	if (date.getDate() >= 30) {
 		japaneseDate += kanjiNumbers[2] + kanjiNumbers[9] + (kanjiNumbers[(date.getDate() % 30) - 1] + "日");
-	}
-	if (date.getDate() >= 20) {
+	} else if (date.getDate() >= 20) {
 		japaneseDate += kanjiNumbers[1] + kanjiNumbers[9] + (kanjiNumbers[(date.getDate() % 20) - 1] + "日");
-	}
-	if (date.getDate() > 10) {
+	} else if (date.getDate() > 10) {
 		japaneseDate += kanjiNumbers[9] + (kanjiNumbers[(date.getDate() % 10) - 1] + "日");
 	} else {
 		japaneseDate += (kanjiNumbers[date.getDate() - 1] + "日");
 	}
 	const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+	const japaneseDaysOfWeek = ["月","火","水","木","金","土","日"];
+	for (let i = 0; i < daysOfWeek.length; i++) {
+		if (splitDate[0] === daysOfWeek[i]) {
+			japaneseDate += "(" + japaneseDaysOfWeek[i] + ")";
+		}
+	}
 	document.getElementById("japanese-date").textContent = japaneseDate;
 }
 convertToJapaneseDate(formattedDate);
