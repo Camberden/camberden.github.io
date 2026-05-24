da66d5e510631107070e5748b6cc4a6f199eb246
Camberden <42500247+Camberden@users.noreply.github.com>
2026-05-23 22:38:21 -0400

    further work on shell file; most options enabled

--- runner.sh
+++ runner.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# % ===== DISPLAY DIVISION ===== % #
 
 timerunner () {
 	rt=$(tput sgr0); r=$(tput setaf 1); g=$(tput setaf 2); y=$(tput setaf 3); c=$(tput setaf 6); b=$(tput bold);
@@ -14,17 +15,30 @@ lang-paths () {
 	echo -n "$X =====> "; which $X ;
 	done
 }
-cyan-echo () {
-	tput setaf 6 ; echo "$1" ; tput sgr0 ;
+
+black-echo () {
+	tput setaf 0 ; echo "$1" ; tput sgr0 
 }
-yellow-echo () {
-	tput setaf 3 ; echo "$1" ; tput sgr0 ;
+red-echo () {
+	tput setaf 1 ; echo "$1" ; tput sgr0 
 }
 green-echo () {
 	tput setaf 2 ; echo "$1" ; tput sgr0 ;
 }
+yellow-echo () {
+	tput setaf 3 ; echo "$1" ; tput sgr0 ;
+}
+blue-echo () {
+	tput setaf 4 ; echo "$1" ; tput sgr0 
+}
 pink-echo () {
 	tput setaf 5 ; echo "$1" ; tput sgr0 
+}
+cyan-echo () {
+	tput setaf 6 ; echo "$1" ; tput sgr0 ;
+}
+white-echo () {
+	tput setaf 7 ; echo "$1" ; tput sgr0 
 }
 closeout () {
 	#@ $1 Closeout? Rerun? true | false
@@ -57,7 +71,7 @@ build-echo() {
 		printf '%s' "${u}${c}${buildstr:$i:1}${rt}" ;
 		sleep 0.01 ;
 	done
-	sleep 0.5;
+	sleep 0.2;
 }
 build-slowly() {
 	#@ $1 $string()
@@ -70,11 +84,10 @@ build-slowly() {
 		printf '%s' "${u}${c}${buildstr:$i:1}${rt}" ;
 		sleep 0.2 ;
 	done
-	sleep 0.5;
+	sleep 0.2;
 	echo ;
 }
 notes-do () {
-	
 	echo "
 	# 0	Black
 	# 1	Red
@@ -110,6 +123,7 @@ notes-do () {
 	"
 }
 
+# % ===== ENVIRONMENT DIVISION ===== % #
 timerunner
 IFS=,;
 local_user=$USER \
@@ -124,71 +138,99 @@ remote_user="$( yq '.configuration.remote.user' runner.yaml)" ;
 remote_host="$( yq '.configuration.remote.host' runner.yaml)" ;
 remote_dir="$( yq '.configuration.remote.dir' runner.yaml)" ;
 ssh_port="$( yq '.configuration.remote.ssh-port' runner.yaml)" ;
-# git_enabled="$( yq '.configuration.git.enabled' runner.yaml)" ;
-# git_dry_run="$( yq '.configuration.git.dry-run' runner.yaml)" ;
+git_enabled="$( yq '.configuration.git.enabled' runner.yaml)" ;
+git_dry_run="$( yq '.configuration.git.dry-run' runner.yaml)" ;
 rsync_enabled="$( yq '.configuration.rsync.enabled' runner.yaml)" ;
 rsync_pull="$( yq '.configuration.rsync.pull' runner.yaml)" ;
-dry_run="$( yq '.configuration.rsync.dry-run' runner.yaml)" ;
+rsync_dry_run="$( yq '.configuration.rsync.dry-run' runner.yaml)" ;
 remote_path="$remote_user$remote_host:$remote_dir" ;
-
 
 git_add () {
 	#@ $1 GIT Dry Run? true | false
-	#@ $2 Rsync Enabled? true | false
-	if $1 ; then
-		build-echo "=====> [GIT DRY RUN] =====> " 2;
-		echo ;
-		git status ;
-		git add . --dry-run ;
-	else
-		build-echo "=====> [GIT WET RUN] =====> " 3;
-		echo ;
-		git add . ;
-		git status ;
-	fi
+	#@ $2 Rsync Dry Run? true | false
+	#@ $3 GIT Enabled? true | false
+	#@ $4 Rsync Enabled? true | false
+	if $3 ; then
+		if $1 ; then
+			git add . --dry-run ;
+			git status ;
+			build-echo "[=====[GIT ENABLED]===" 2 ;
+			build-echo "==[DRY]=====[NO COMMIT & NO PUSH]==> " 4 ;
+			echo ;
+		else
+			echo ;
+			git add . ;
+			git status ;
+			build-echo "[=====[GIT ENABLED]===" 5 ;
+			build-echo "==[WET]=====[COMMIT & PUSH]==> " 3 ;
+			echo ;
+		fi
 
-	if $2 ; then
-		build-echo "=====> [RSYNC ENABLED] =====> " 3;
-		echo ;
+		if $4 ; then
+			build-echo "[=====[RSYNC ENABLED]===" 3 ;
+			if $2 ; then
+			build-echo "==[DRY]=====[SAFE MODE]==> " 2 ;
+			echo ;
+			else
+			build-echo "==[WET]=====[OVERWRITE MODE]==> " 1 ;
+			echo ;
+			fi
+		else
+			build-echo "=====> [RSYNC DISABLED] =====> " 2;
+			echo ;
+		fi
+
 	else
-		build-echo "=====> [RSYNC DISABLED] =====> " 2;
-		echo ;
+	build-echo "=====> [GIT DISABLED] =====> " 7;
+	echo ;
 	fi
 }
 
 git_commit () {
 	#@ $1 GIT Dry Run? true | false
 	#@ $2 Message? string() from $message
-	if $1 ; then
-		git commit -m "$2" --dry-run ;
+	#@ $3 GIT Enabled? true | false
+	if $3 ; then
+		if $1 ; then
+			git commit -m "$2" --dry-run ;
+		else
+			git commit -m "$2" ;
+		fi
 	else
-		git commit -m "$2" ;
+	echo "$2" ;
 	fi
 }
 
 rsync_push () {
-	#@ $1 Dry Run? true | false
-	#@ $2 Rsync? true | false
-	if $1 ; then 
-		cyan-echo "=====> DRY GIT PUSHED THE FOLLOWING: =====>" ;
-		git push --dry-run ;
-		yellow-echo "=====> DRY COMMIT & PUSH COMPLETE =====>" ;
-		if $2 ; then
-		cyan-echo "=====> DRY RSYNCED TO SERVER: =====> " ; #-havzune
-		rsync -havzun -e "ssh -p $ssh_port" --exclude-from=.gitignore ./* "$remote_path" ;
-		cyan-echo "=====> DRY COMMIT, PUSH, & RSYNC COMPLETE =====>/" ;
+	#@ $1 Git Dry Run? true | false
+	#@ $2 Rsync Dry Run? true | false
+	#@ $3 Git Enabled? true | false
+	#@ $4 Rsync Enabled? true | false
+
+	if $3 ; then
+		if $1 ; then 
+			green-echo "=====> DRY GIT PUSHED THE FOLLOWING: =====>" ;
+			git push --dry-run ;
+			cyan-echo "=====> DRY COMMIT & PUSH COMPLETE =====>" ;
+			else
+			cyan-echo "=====> GIT PUSHED THE FOLLOWING: =====> " ;
+			git push ;
+			yellow-echo "=====> GIT COMMIT & PUSH COMPLETE =====>" ;
 		fi
-		closeout ;
-	else
-		cyan-echo "=====> GIT PUSHED THE FOLLOWING: =====> "
-		git push ;
-		yellow-echo "=====> GIT COMMIT & PUSH COMPLETE =====>" ;
+
+	if $4 ; then
 		if $2 ; then
-		cyan-echo "=====> RSYNCED TO SERVER: =====> " ; #-havzue
-		rsync -havzu -e "ssh -p $ssh_port" --exclude-from=.gitignore ./* "$remote_path" 
-		yellow-echo "=====> RSYNC COMPLETE =====>" ;
+			green-echo "=====> DRY RSYNC TO SERVER: =====> " ; #-havzune
+			rsync -havzun -e "ssh -p $ssh_port" --exclude-from=.gitignore ./* "$remote_path" ;
+			cyan-echo "=====> DRY RSYNC COMPLETE =====>/" ;
+			closeout ;
+		else
+			pink-echo "=====> RSYNC TO SERVER: =====> " ; #-havzue
+			rsync -havzu -e "ssh -p $ssh_port" --exclude-from=.gitignore ./* "$remote_path" ;
+			yellow-echo "=====> RSYNC COMPLETE =====>" ;
+			fi
+			closeout ;
 		fi
-		closeout ;
 	fi
 }
 
@@ -196,39 +238,46 @@ rsync_pull () {
 	#@ $1 Dry Run? true | false
 	#@ $2 Rsync Pull? true | false
 	if $1 ; then
-		cyan-echo "=====> DRY RSYNCED PULLED FROM SERVER: =====> " ;
+		cyan-echo "=====> DRY RSYNC PULL FROM SERVER: =====> " ;
 		rsync -avh --dry-run -e "ssh -p $ssh_port" "$remote_path" "$local_dir" ;
 		yellow-echo "=====> Dry RSYNC Complete =====>" ;
 		# yq -i .configuration.rsync.pull=false runner.yaml ;
 		closeout ;
 	fi
 	if $2 ; then
-		cyan-echo "=====> RSYNCED PULLED FROM SERVER: =====> " ;
+		cyan-echo "=====> RSYNC PULL FROM SERVER: =====> " ;
 		rsync -avh --exclude "*.git" -e "ssh -p $ssh_port" "$remote_path" "$local_dir" ;
 		# rsync -chavzuP --ignore-times -e "ssh -p $ssh_port" --exclude-from=.gitignore "$remote_path" ./* ;
 		yellow-echo "=====> RSYNC Complete =====>" ;
-		# yq -i .configuration.rsync.pull=false runner.yaml ;
+		yq -i .configuration.rsync.pull=false runner.yaml ;
 		closeout ;
 	fi
 }
-
+# % ================================================================= % #
+# % ==================== PROCEDURE DIVISION ========================= % #
+# % ================================================================= % #
 yellow-echo "SOURCE DIRECTORY =====> $local_dir" ;
 yellow-echo "TARGET DIRECTORY =====> $remote_path" ;
 
 if "$rsync_pull" ; then
 	build-echo "=====> [PREPARING FOR SERVER RSYNC PULL] =====> " 1;
-	if "$dry_run" ; then
+	echo ;
+	if "$rsync_dry_run" ; then
 		build-echo "[Dry TESTING Pull Mode Enabled] =====> " 3;
-	else 
+		echo ;
+		else 
 		build-echo "[RSYNC " 6 ; build-slowly "LIVE" 6 ; build-echo " STANDING BY] =====> " 1;
+		echo ;
 	fi
+
 		build-echo "[Press r to proceed with server pull from target directory] =====> " 5;
+		echo ;
 		read -r serverpull ;
 		serverpull="$(printf '%s' "$serverpull" )" ;
 
 	if [ "$serverpull" == "r" ] ; then
 		build-echo "=====> [SERVER RSYNC PULL ACTIVATED] =====> " 3;
-		rsync_pull "$dry_run" "$rsync_pull" ;
+		rsync_pull "$rsync_dry_run" "$rsync_pull" ;
 	else
 		build-echo "=====> Server pull cancelled and disabled. Rerun utility to enable." 2;
 		yq -i .configuration.rsync.pull=false runner.yaml
@@ -237,41 +286,119 @@ if "$rsync_pull" ; then
 	
 fi
 
-git_add "$dry_run" "$rsync_enabled" ;
-build-echo "=====> Enter your commit message, " 6 ; build-slowly "$local_user". ;
-cyan-echo "[Cancels under 3 characters.] =====> " ;
+git_add "$git_dry_run" "$rsync_dry_run" "$git_enabled" "$rsync_enabled";
 
-if "$dry_run" ; then
-build-echo "[Press w to enable wet run (overwrite mode)] =====> " 5;
-else 
-build-echo "[Press d to enable dry run (safe mode)] =====> " 5;
+if "$git_enabled" ; then
+build-echo "=====> Enter your commit message, " 6 ; build-slowly "$local_user". ;
+	if "$git_dry_run" ; then
+	cyan-echo "[Press h to enable Git Wet Run] =====> " ;
+	echo ;
+	else 
+	cyan-echo "[Press t to enable Git Dry Run] =====> " ;
+	echo ;
+	fi 
+else
+build-echo "=====> Input command, " 6 ; build-slowly "$local_user". ;
+build-echo "[Press g to enable Git] =====> " 5 ;
+fi
+if "$rsync_enabled" ; then
+cyan-echo "[Press z to disable RSYNC ] =====> " ;
+	if "$rsync_dry_run" ; then
+		cyan-echo "[Press w to enable RSYNC Wet run (overwrite mode)] =====> " ;
+		build-echo "[Cancels under 3 characters] =====> " 4 ;
+	else 
+		cyan-echo "[Press x to enable RSYNC Pull from Server] =====> " ;
+		cyan-echo "[Press d to enable RSYNC Dry run (safe mode)] =====> " ;
+		build-echo "[Cancels under 3 characters] =====> " 4 ;
+	fi
+else
+blue-echo "[Press s to enable RSYNC ] =====> " ;
+build-echo "[Cancels under 3 characters] =====> " 4 ;
 fi
 
 read -r message ;
-message="$(printf '%s' "$message" )" ;
+message="$(printf '%s' "$message" )"
 
-if [ "$dry_run" ] && [ "$message" == "w" ] ; then
-	build-echo "=====> [WET RUN (OVERWRITE MODE) ENABLED] =====> " 3;
+if [ "$message" == "" ] ; then
+git reset ;
+cyan-echo "=====> Push cancelled..." ; closeout ;
+fi
+if [ "$rsync_dry_run" ] && [ "$message" == "w" ] ; then
+	build-echo "=====> [RSYNC WET RUN (OVERWRITE MODE) ENABLED] =====> " 3 ;
 	git reset ;
-	pink-echo "=====> Rerun enabled utility. Exiting..." ;
+	pink-echo "=====> Rerunning rsync enabled utility. Exiting..." ;
+	yq -i .configuration.rsync.enabled=true runner.yaml
 	yq -i .configuration.rsync.dry-run=false runner.yaml
 	closeout false ;
 fi
-
-if [ "$dry_run" == false ] && [ "$message" == "d" ] ; then
-	build-echo "=====> [DRY RUN (SAFE MODE) ENABLED] =====> " 2;
+if [ "$rsync_dry_run" == false ] && [ "$message" == "d" ] ; then
+	build-echo "=====> [RSYNC DRY RUN (SAFE MODE) ENABLED] =====> " 2;
 	git reset ;
-	pink-echo "=====> Rerun utility for safe testing. Exiting..." ;
+	pink-echo "=====> Rerunning utility for safe testing. Exiting..." ;
+	yq -i .configuration.rsync.enabled=true runner.yaml
 	yq -i .configuration.rsync.dry-run=true runner.yaml
 	closeout false ;
 fi 
 
-if [ ${#message} -gt 2 ] ; then
+
+
+if [ "$rsync_enabled" == true ] && [ "$message" == "z" ] ; then
+	build-echo "=====> [RSYNC DISABLED] =====> " 3;
+	git reset ;
+	pink-echo "=====> Rerunning with rsync disabled. Exiting..." ;
+	yq -i .configuration.rsync.enabled=false runner.yaml
+	yq -i .configuration.rsync.pull=false runner.yaml
+	yq -i .configuration.rsync.dry-run=false runner.yaml
+	closeout false ;
+fi 
+if [ "$rsync_enabled" == false ] && [ "$message" == "s" ] ; then
+	build-echo "=====> [RSYNC RE-ENABLED] =====> " 3;
+	git reset ;
+	pink-echo "=====> Rerunning with rsync enabled. Exiting..." ;
+	yq -i .configuration.rsync.enabled=true runner.yaml
+	closeout false ;
+fi 
+if [ "$rsync_enabled" == true ] && [ "$message" == "x" ] ; then
+	build-echo "=====> [RSYNC PULL ENABLED] =====> " 3;
+	git reset ;
+	pink-echo "=====> Rerunning under pull procedure. Exiting..." ;
+	yq -i .configuration.rsync.pull=true runner.yaml
+	closeout false ;
+fi 
+
+
+if [ "$git_enabled" == false ] && [ "$message" == "g" ] ; then
+	build-echo "=====> [GIT ENABLED] =====> " 7;
+	git reset ;
+	pink-echo "=====> Rerunning utility with git. Exiting..." ;
+	yq -i .configuration.git.enabled=true runner.yaml
+	yq -i .configuration.git.dry-run=false runner.yaml
+
+	closeout false ;
+fi
+if [ "$git_enabled" == true ] && [ "$message" == "t" ] ; then
+	build-echo "=====> [GIT DRY RUN ENABLED] =====> " 7;
+	git reset ;
+	pink-echo "=====> Rerunning, git commits and pushes are disabled. Exiting..." ;
+	yq -i .configuration.git.enabled=true runner.yaml
+	yq -i .configuration.git.dry-run=true runner.yaml
+
+	closeout false ;
+fi
+if [ "$git_enabled" == true ] && [ "$message" == "h" ] ; then
+	build-echo "=====> [GIT COMMIT & PUSH ENABLED] =====> " 7;
+	git reset ;
+	pink-echo "=====> Rerunning, git now commits and pushes. Exiting..." ;
+	yq -i .configuration.git.enabled=true runner.yaml
+	yq -i .configuration.git.dry-run=false runner.yaml
+
+	closeout false ;
+fi
+if [ "$git_enabled" == true ] && [ ${#message} -gt 2 ] ; then
 	echo "[SUCCESS] =====> "
 	commit=$message\
 	yq -i '.configuration.meta.latest-commit = env(commit)' runner.yaml ;
-	git_commit "$dry_run" "$message" ;
-
+	git_commit "$git_dry_run" "$message" "$git_enabled" ;
 	else
 	git reset ;
 	echo "=====> Too few characters (# < 3) Exiting..."
@@ -279,14 +406,19 @@ if [ ${#message} -gt 2 ] ; then
 fi
 
 cyan-echo "=====> Push data? "
-build-echo "[Press p to confirm] =====> " 6;
+cyan-echo "[Press p to confirm] =====> " 6;
+build-echo "[Other inputs cancel process.] =====> " ;
+
 read -r pushed
 
 if [ "$pushed" == "p" ] ; then
-	echo "Data pushing: " ; pink-echo "$message" ;
-	rsync_push "$dry_run" "$rsync_enabled" ;
-
+	echo "Data pushing: " ; echo ; pink-echo "$message" ; echo ;
+	rsync_push "$git_dry_run" "$rsync_dry_run" "$git_enabled" "$rsync_enabled" ;
 	else
 	git reset ;
 	cyan-echo "=====> Push cancelled..." ; closeout ;
 fi
+#@ $1 Git Dry Run? true | false
+#@ $2 Rsync Dry Run? true | false
+#@ $3 Git Enabled? true | false
+#@ $4 Rsync Enabled? true | false
